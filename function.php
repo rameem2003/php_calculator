04e54da0498995eefbfa62142e09a92f2fbf6060
rameem2003 <rameem2019@gmail.com>
2022-03-01 09:56:14 +0600

    PHP Calculator

---
+++ function.php
@@ -0,0 +1,42 @@
+<?php 
+
+    if(isset($_POST["submit"])){
+        $a = $_POST["f_num"];
+        $b = $_POST["s_num"];
+
+        if($a != "" && $b != ""){
+            $op = $_POST["op"];
+
+            switch($op){
+                case "add":
+                    $add = $a + $b;
+                    echo "The addition of {$a} + {$b} is {$add}";
+                    break;
+
+                case "sub":
+                    $sub = $a - $b;
+                    echo "The submission of {$a} - {$b} is {$sub}";
+                    break;
+
+
+                case "mul":
+                    $mul = $a * $b;
+                    echo "The multiplication of {$a} * {$b} is {$mul}";
+                    break;
+    
+                case "div":
+                    $div = $a / $b;
+                    echo "The divition of {$a} / {$b} is {$div}";
+                    break;
+
+                default:
+                    echo "Wrong!!";
+            }
+        }
+
+        else{
+            echo "ভালো হয়ে যাও মাসুদ !! ";
+        }
+    }
+
+?>
---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Calculator</title>
+
+    <!-- local CSS -->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section id="calculator">
+        <form action="" method="POST">
+            <h1>PHP Calculator</h1>
+
+            <input type="number" name="f_num" id="" placeholder="Enter the first numner: ">
+            <input type="number" name="s_num" id="" placeholder="Enter the second number: ">
+
+
+            <select name="op" id="">
+                <option value="add">Addition</option>
+                <option value="sub">Submission</option>
+                <option value="mul">Multiplication</option>
+                <option value="div">Division</option>
+            </select>
+
+            <input type="submit" name="submit" value="Run >>>">
+
+            <p class="output"> <?php include "function.php";  ?> </p>
+        </form>
+    </section>
+
+
+    <footer>
+        <span>&copy; Mahmood Hassan Rameem.</span>
+    </footer>
+</body>
+</html>
